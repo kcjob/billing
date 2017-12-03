b0d92d44609df2de4ea8fb17814cfb4acc792644
kcjob <30427093+kcjob@users.noreply.github.com>
2017-12-03 13:50:56 -0500

    Update start.php
    Remove commented lines of code

--- start.php
+++ start.php
@@ -4,7 +4,6 @@ require_once(__DIR__ .'/vendor/autoload.php');
 use \Apps\BillingInformation;
 use \Apps\AccessCSVData;
 use \Apps\BillObject;
-//use \Apps\EmailMessageDataObject;
 use \Apps\MessageDataFromBillObject;
 use \Apps\TemplateView;
 
@@ -17,14 +16,3 @@ $arrayOfMsgDataObjects = MessageDataFromBillObject::getMessageData($arrayofbillO
 $template = new TemplateView();
 $template -> generateView($arrayOfMsgDataObjects);
 
-/* $loader = new Twig_Loader_Filesystem('Templates');
-$twig = new Twig_Environment($loader, array('debug' => true));
-$twig->addExtension(new Twig_Extension_Debug());
-$template = $twig->load('messagebody.html.twig');
-
-//----- split out each msg object
-foreach($arrayOfMsgDataObjects as $msgDataObject){
-//var_dump($dataObject->userName);
-echo $template->render(array('dataObject'=>$msgDataObject));
-}
-*/
