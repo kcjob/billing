e4c1ad00f3bc629736b874bdc810df071a29464d
kcjob <30427093+kcjob@users.noreply.github.com>
2017-12-04 17:13:53 -0500

    Clean up of code

--- start.php
+++ start.php
@@ -16,24 +16,3 @@ $csvDataArray = AccessCSVData::getCsvDataAsArray();
 $arrayofbillObjects = BillObject::createBillObject($csvDataArray);
 $arrayOfMsgDataObjects = MessageDataFromBillObject::getMessageData($arrayofbillObjects);
 EmailMessageGenerator::createEmail($arrayOfMsgDataObjects);
-
-
-
-
-
-//echo $msg;
-/*
-$template = new TemplateView();
-$template -> generateView($arrayOfMsgDataObjects);
-
- $loader = new Twig_Loader_Filesystem('Templates');
-$twig = new Twig_Environment($loader, array('debug' => true));
-$twig->addExtension(new Twig_Extension_Debug());
-$template = $twig->load('messagebody.html.twig');
-
-//----- split out each msg object
-foreach($arrayOfMsgDataObjects as $msgDataObject){
-//var_dump($dataObject->userName);
-echo $template->render(array('dataObject'=>$msgDataObject));
-}
-*/
